16c7ab6860ee9bdd34685364bd72b4056a2057f6
Nawaz <nawaz4jaffar@hotmail.com>
2021-08-23 23:49:49 +0530

    my project

---
+++ src/test/java/mavenjava/LoginTest.java
@@ -0,0 +1,14 @@
+package mavenjava;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+@Test
+public void login() {
+
+
+System.out.println("Login");
+}
+
+}
---
+++ src/test/java/mavenjava/LogoutTest.java
@@ -0,0 +1,14 @@
+package mavenjava;
+
+import org.testng.annotations.Test;
+
+public class LogoutTest {
+	
+@Test
+	public void logout() {
+
+
+	System.out.println("Logout");
+	}
+
+}
